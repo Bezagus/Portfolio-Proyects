de8d32676887aa6bbd2c72136558431a746b947c
Agustin David Benitez <98838718+Bezagus@users.noreply.github.com>
2023-04-17 14:49:22 -0300

    Update Footer.jsx

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer(){
             <h3>Mis Redes:</h3>
             <ul>
                 <li><a href="https://github.com/Bezagus" target="_blank"><img src={IconGitHub} alt="GitHub Icon" /></a></li>
-                <li><a href="https://www.linkedin.com/in/agustin-benitez-271b94241" target="_blank"><img src={IconLinkdin} alt="LinkdIn Icon" /></a></li>
+                <li><a href="https://www.linkedin.com/in/agustin-david-benitez/" target="_blank"><img src={IconLinkdin} alt="LinkdIn Icon" /></a></li>
                 <li><a href="https://www.instagram.com/agus_bez/" target="_blank"><img src={IconInstagram} alt="Instagram Icon" /></a></li>
                 <li><a href="https://api.whatsapp.com/send/?phone=%2B542972527279&text&type=phone_number&app_absent=0" target="_blank"><img src={IconWhatsApp} alt="WhatsApp Icon" /></a></li>
                 <li><a href="https://www.youtube.com/channel/UC6JvQsrhTbVTeq3bb3gfP6Q" target="_blank"><img src={IconYoutube} alt="Youtube Icon" /></a></li>
